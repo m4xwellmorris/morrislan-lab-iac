7b88666502a16a9645bb60093148995771fe912d
root <root@ops-prod.morris.lan>
2022-01-04 17:19:18 +0000

    Fixed cloud-init variable resolution

--- config.xenorchestra.tf
+++ config.xenorchestra.tf
@@ -2,20 +2,16 @@ data "xenorchestra_pool" "prod" {
   name_label = "morrislan-prod"
 }
 
-data "xenorchestra_template" "default" {
+data "xenorchestra_template" "ubuntu2004img" {
   name_label = "ubuntu2004img"
 }
 
-data "xenorchestra_sr" "rootdrives" {
-  name_label = "nosan-rootdrives"
+data "xenorchestra_sr" "nosan_rootdrives" {
+  name_label = "nosan_rootdrives"
   pool_id = data.xenorchestra_pool.prod.id
 }
 
 data "xenorchestra_network" "lan" {
   name_label = "LAN"
   pool_id = data.xenorchestra_pool.prod.id
-}
-
-data "xenorchestra_cloud_config" "cloud_config" {
-  name = "Default-UserConfig"
 }
